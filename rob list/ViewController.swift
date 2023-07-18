622fcfa413acc43399520a1a1d5483f894b26f02
viktab <vikt@mit.edu>
2023-07-17 23:18:27 -0700

    Initial Commit

---
+++ rob list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rob list
+//
+//  Created by Viktoriya Tabunshchyk on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rob listUITests/rob_listUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rob_listUITestsLaunchTests.swift
+//  rob listUITests
+//
+//  Created by Viktoriya Tabunshchyk on 7/17/23.
+//
+
+import XCTest
+
+final class rob_listUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
